d512ca30be65a58b847de22da56a2138520319e9
Anuj Yadav <anujyiitk@gmail.com>
2026-07-25 13:38:10 +0530

    Time: 0 ms (100%), Space: 12.7 MB (30.74%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,12 +5,27 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
+
+
         n = len(matrix)
         m = len(matrix[0])
-        for i in range(n):
-            for j in range(m):
-                if matrix[i][j] == target:
-                    return True
-        return False
+        # for i in range(n):
+        #     for j in range(m):
+        #         if matrix[i][j] == target:
+        #             return True
+        # return False
 
-        
+        # Time Complexity O(m*n)
+        low = 0
+        high = m * n - 1
+        while low <= high:
+            mid = low + (- low + high) // 2
+
+            if matrix[mid / m][mid %m] == target:
+                return True
+            elif matrix[mid / m][mid %m] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
+            
