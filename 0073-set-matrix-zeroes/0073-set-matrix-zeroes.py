6dd1a26311cc308d8b202cc19264cc79c9c860fc
Anuj Yadav <anujyiitk@gmail.com>
2026-07-25 23:03:45 +0530

    Time: 56 ms (17.58%), Space: 14 MB (8.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,19 +4,50 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
-        m = len(matrix[0])
-        zero_row = [False] * n
-        zero_col = [False] * m
+        # n = len(matrix)
+        # m = len(matrix[0])
+        # zero_row = [False] * n
+        # zero_col = [False] * m
 
-        for i in range(n):
-            for j in range(m):
+        # for i in range(n):
+        #     for j in range(m):
+        #         if matrix[i][j] == 0:
+        #             zero_row[i] = True
+        #             zero_col[j] = True
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if zero_row[i] or zero_col[j]:
+        #             matrix[i][j] = 0
+        # return matrix  
+        # Time Complexity O(m * n) and Space Complexity O(m + n)
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for i in range(m):
+            for j in range(n):
                 if matrix[i][j] == 0:
-                    zero_row[i] = True
-                    zero_col[j] = True
-
-        for i in range(n):
-            for j in range(m):
-                if zero_row[i] or zero_col[j]:
+                    self.helper(matrix, i - 1, j, "U")
+                    self.helper(matrix, i + 1, j, "D")
+                    self.helper(matrix, i, j - 1, "L")
+                    self.helper(matrix, i , j + 1, "R")
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == "*":
                     matrix[i][j] = 0
-        return matrix  
+        return matrix
+
+    def helper(self, matrix, m, n, direction):
+        if (0 <= m < len(matrix)) and (0 <= n < len(matrix[0])) and matrix[m][n] != 0:
+            matrix[m][n] = "*"
+
+            if direction == "U":
+                self.helper(matrix, m - 1, n, "U")
+            elif direction == "D":
+                self.helper(matrix, m + 1, n, "D")
+            elif direction == "L":
+                self.helper(matrix, m, n - 1, "L")
+            else:
+                self.helper(matrix, m , n + 1, "R")
