383a2fe2564d4507349bb2086fcc9091ff6a6553
Anuj Yadav <anujyiitk@gmail.com>
2026-07-25 13:56:04 +0530

    Time: 3 ms (91.03%), Space: 12.8 MB (33.97%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -5,13 +5,23 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(mat)
-        count_1 = []
+        # count_1 = []
+
+        # for i in range(n):
+        #     count_1.append(sum(mat[i]))
+        # max_ones = max(count_1)
+        # if  max_ones > 0:
+        #     return [count_1.index(max_ones), max_ones]
+        # return [0, 0]
+
+        max_row_idxx = 0
+        max_ones_count = -1
 
         for i in range(n):
-            count_1.append(sum(mat[i]))
-        max_ones = max(count_1)
-        if  max_ones > 0:
-            return [count_1.index(max_ones), max_ones]
-        return [0, 0]
+            ones_in_row = sum(mat[i])
+            if ones_in_row > max_ones_count:
+                max_ones_count = ones_in_row
+                max_row_idx = i
+        return [max_row_idx, max_ones_count]
         
         
