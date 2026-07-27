e09a7cf6707c04d51937faf2009858b9ae5dcd9d
Anuj Yadav <anujyiitk@gmail.com>
2026-07-27 16:57:44 +0530

    Time: 0 ms (100%), Space: 12.5 MB (6.9%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -3,8 +3,21 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        """
+        
         n = len(nums)
         nums.sort()
         return (nums[n - 1] - 1) * (nums[n - 2] - 1)
+        Time complexity: O(nlogn)
+
+        """
+
+        first = second = 0
         
+        for num in nums:
+            if num > first:
+                second = first
+                first = num
+            elif num > second:
+                second = num
+                
+        return (first - 1) * (second - 1)
