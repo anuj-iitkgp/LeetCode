f39d9fae98f629800447702d3a25793ad3df9555
Anuj Yadav <anujyiitk@gmail.com>
2026-07-28 16:02:58 +0530

    Time: 59 ms (74.44%), Space: 42.3 MB (87.59%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,38 +1,39 @@
 class Solution(object):
     def rearrangeArray(self, nums):
-        # even = 0
-        # odd = 1
+        even = 0
+        odd = 1
+        res = [0] * len(nums)
 
-        # while even < len(nums) and odd < len(nums) :
-        #     if nums[odd] > 0 and nums[even] < 0:
-        #         nums[odd], nums[even] = nums[even], nums[odd]
-        #         odd += 1
-        #         even += 2
-        #     else:
-        #         even += 2
-   
-        # return nums
-
-        pos = []
-        neg = []
-
-        for num in nums:
-            if num < 0:
-                neg.append(num)
+        for num in nums :
+            if num > 0:
+                res[even] = num
+                even += 2
             else:
-                pos.append(num)
-        
-        i, j = 0, 0
-        res = []
-        while i < len(pos) and j < len(neg):
-            res.append(pos[i])
-            i += 1
-            res.append(neg[j])
-           
-            j += 1
-            
-            
+                res[odd] = num
+                odd += 2
+   
         return res
+
+        # pos = []
+        # neg = []
+
+        # for num in nums:
+        #     if num < 0:
+        #         neg.append(num)
+        #     else:
+        #         pos.append(num)
+        
+        # i, j = 0, 0
+        # res = []
+        # while i < len(pos) and j < len(neg):
+        #     res.append(pos[i])
+        #     i += 1
+        #     res.append(neg[j])
+           
+        #     j += 1
+            
+            
+        # return res
 
 
             
