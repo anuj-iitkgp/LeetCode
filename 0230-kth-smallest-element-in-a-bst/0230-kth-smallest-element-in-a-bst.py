8c34752ea1939deaf44b73d84c13b772ffaaaf37
Anuj Yadav <anujyiitk@gmail.com>
2026-08-03 23:34:24 +0530

    Time: 0 ms (100%), Space: 20.3 MB (35.91%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -8,11 +8,34 @@ class Solution(object):
     def kthSmallest(self, root, k):
     
 
-        def inorderTraversal(root):
-            if not root:
-                return []
-            return inorderTraversal(root.left) + [root.val] + inorderTraversal(root.right)
+        # def inorderTraversal(root):
+        #     if not root:
+        #         return []
+        #     return inorderTraversal(root.left) + [root.val] + inorderTraversal(root.right)
         
-        val = inorderTraversal(root)
-        return val[k - 1]
+        # val = inorderTraversal(root)
+        # return val[k - 1]
+
+
+        # 2nd approach
+
+        count = [k]
+        ans = [0]
+
+        def dfs(node):
+            if not node:
+                return
+            
+            dfs(node.left)
+
+            if count[0] == 1:
+                ans[0] = node.val
+            
+            count[0] -= 1
+
+            if count[0] > 0:
+                dfs(node.right)
+            
+        dfs(root)
+        return ans[0]
             
