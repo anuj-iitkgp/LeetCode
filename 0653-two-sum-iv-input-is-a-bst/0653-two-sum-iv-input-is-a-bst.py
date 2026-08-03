ff9fd7464a547471a5ddd0d96522455b7922931c
Anuj Yadav <anujyiitk@gmail.com>
2026-08-04 01:09:22 +0530

    Time: 4 ms (91.26%), Space: 18.8 MB (33.01%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,26 +6,43 @@
 #         self.right = right
 class Solution(object):
     def findTarget(self, root, k):
-        if not root:
-            return True
+
+# First approach: inorder + 2 sum problem
+
+        # if not root:
+        #     return True
         
             
-        def inorder(root):
-            if not root:
-                return []
-            return inorder(root.left) + [root.val] + inorder(root.right)
+        # def inorder(root):
+        #     if not root:
+        #         return []
+        #     return inorder(root.left) + [root.val] + inorder(root.right)
         
-        val = inorder(root)
-        l = 0
-        r = len(val) - 1
+        # val = inorder(root)
+        # l = 0
+        # r = len(val) - 1
 
-        while l < r:
-            sum = val[l] + val[r]
-            if sum < k:
-                l += 1
-            elif sum > k:
-                r -= 1
-            else:
-                return True
+        # while l < r:
+        #     sum = val[l] + val[r]
+        #     if sum < k:
+        #         l += 1
+        #     elif sum > k:
+        #         r -= 1
+        #     else:
+        #         return True
         
-        return False
+        # return False
+
+#   2nd approach using hashset
+
+        seen = set()
+
+        def dfs(root):
+            if not root:
+                return False
+            
+            if (k - root.val) in seen:
+                return True
+            seen.add(root.val)
+            return dfs(root.left) or dfs(root.right)
+        return dfs(root)
