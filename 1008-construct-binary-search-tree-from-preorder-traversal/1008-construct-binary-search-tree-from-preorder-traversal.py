05a9dca8596061e3a4a846aff649d34c087b02d5
Anuj Yadav <anujyiitk@gmail.com>
2026-08-04 00:30:15 +0530

    Time: 0 ms (100%), Space: 12.6 MB (34.1%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -5,26 +5,50 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+
     def bstFromPreorder(self, preorder):
-        if not preorder:
-            return []
-        
-        root_val = preorder[0]
-        root = TreeNode(root_val)
 
-        for val in preorder[1:]:
-            self.insertBST(root, val)
-        return root
-
-    def insertBST(self, root, val):
-        if not root:
-            return TreeNode(val)
+    # First Approach ( time O(N^2)  worst-case,  O(N \log N)  average, space O(H) )
+    #     if not preorder:
+    #         return []
         
-        if root.val < val:
-            root.right = self.insertBST(root.right, val)
-        else:
-            root.left = self.insertBST(root.left, val)
-        return root
+    #     root_val = preorder[0]
+    #     root = TreeNode(root_val)
+
+    #     for val in preorder[1:]:
+    #         self.insertBST(root, val)
+    #     return root
+
+    # def insertBST(self, root, val):
+    #     if not root:
+    #         return TreeNode(val)
+        
+    #     if root.val < val:
+    #         root.right = self.insertBST(root.right, val)
+    #     else:
+    #         root.left = self.insertBST(root.left, val)
+    #     return root
+
+    # 2nd Approach
+
+        self.idx = 0
+
+        def build(bound = float('inf')):
+            if self.idx >= len(preorder) or preorder[self.idx] > bound:
+                return None
+            
+            val = preorder[self.idx]
+            self.idx += 1
+
+            root = TreeNode(val)
+            root.left = build(val) # Left subtree values must be < val
+            root.right = build(bound) # Right subtree values must be < parent bound
+
+            return root
+        return build()
+
+
+
 
 
         
