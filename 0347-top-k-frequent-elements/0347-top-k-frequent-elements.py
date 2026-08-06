28ca60d8fc243c8482647bc62d1550a6af6385f7
Anuj Yadav <anujyiitk@gmail.com>
2026-08-06 18:22:11 +0530

    Time: 11 ms (56.37%), Space: 14.3 MB (42.16%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,31 @@
 from collections import Counter
+import heapq
 
 class Solution(object):
     def topKFrequent(self, nums, k):
-        counts = Counter(nums)
+        # counts = Counter(nums)
         
-        # Sort the (num, count) pairs by count in descending order
-        sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        # # Sort the (num, count) pairs by count in descending order
+        # sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
         
-        output = []
-        for num, count in sorted_counts:
-            output.append(num)
-            if len(output) == k:
-                return output
+        # output = []
+        # for num, count in sorted_counts:
+        #     output.append(num)
+        #     if len(output) == k:
+        #         return output
+        # T.C. O(nlogk) and Space: O(n)
+
+# another approach
+
+        counter = Counter(nums)
+        heap = []
+
+        for key, val in counter.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+        
+        return [h[1] for h in heap]
+
+
