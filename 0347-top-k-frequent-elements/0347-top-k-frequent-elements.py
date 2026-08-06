a2653110d1d2d3abf155ed87c0cc3ee1bc89736b
Anuj Yadav <anujyiitk@gmail.com>
2026-08-06 18:35:12 +0530

    Time: 10 ms (60.36%), Space: 16 MB (26.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -13,19 +13,41 @@ class Solution(object):
         #     output.append(num)
         #     if len(output) == k:
         #         return output
-        # T.C. O(nlogk) and Space: O(n)
+        # T.C. O(nlogn) and Space: O(n)
 
 # another approach
 
-        counter = Counter(nums)
-        heap = []
+        # counter = Counter(nums)
+        # heap = []
 
-        for key, val in counter.items():
-            if len(heap) < k:
-                heapq.heappush(heap, (val, key))
-            else:
-                heapq.heappushpop(heap, (val, key))
+        # for key, val in counter.items():
+        #     if len(heap) < k:
+        #         heapq.heappush(heap, (val, key))
+        #     else:
+        #         heapq.heappushpop(heap, (val, key))
         
-        return [h[1] for h in heap]
+        # return [h[1] for h in heap]
 
+        # T.C. O(nlogk) and Space: O(n)
+
+# optimize approach
+
+        n = len(nums)
+
+        counter = Counter(nums)
+        buckets = [0] * (n + 1)
+
+        for num, freq in counter.items():
+            if buckets[freq] == 0:
+                buckets[freq] = [num]
+            else:
+                buckets[freq].append(num)
+        
+        ans = []
+        for i in range(n, -1, -1):
+            if buckets[i] != 0:
+                ans.extend(buckets[i])
+            if len(ans) == k:
+                break
+        return ans
 
