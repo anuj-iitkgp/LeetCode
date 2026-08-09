94e2274d2ecff9de86dadc5afb1597a8b509c569
Anuj Yadav <anujyiitk@gmail.com>
2026-08-09 15:00:21 +0530

    Time: 230 ms (47.58%), Space: 27.6 MB (93.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,45 @@
+from collections import deque
 class Solution(object):
     def numIslands(self, grid):
+
+# with the help of BFS
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        row = len(grid)
-        col = len(grid[0])
-        isIsland = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        islands = 0
 
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r >= row or c >= col or grid[r][c] == "0"):
-                return
-            
+        def bfs(r, c):
+            q = deque()
             grid[r][c] = "0"
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
+            q.append((r, c))
 
-            
-        for r in range(row):
-            for c in range(col):
+            while q:
+                row, col = q.popleft()
+
+                for dr, dc in directions:
+                    dr, dc = dr + row, dc + col
+
+                    if (dr < 0 or dc < 0 or dr >= rows or dc >= cols or grid[dr][dc] == "0" ):
+                        continue
+                    
+                    q.append((dr, dc))
+                    grid[dr][dc] = "0"
+                    
+
+
+
+
+
+
+        # traverse all the cell of the grid
+
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == "1":
-                    dfs(r, c)
-                    isIsland += 1
+                    bfs(r, c)
+                    islands += 1
+        return islands
 
-        return isIsland 
 
+# Time complexity: O(m*n)
+# Space complexity: O(m*n)
