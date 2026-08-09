b8e00ce123fdeeb6aeb43ffe1f938b39da3422b2
Anuj Yadav <anujyiitk@gmail.com>
2026-08-10 01:26:46 +0530

    Time: 0 ms (100%), Space: 12.2 MB (99.42%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,5 +1,14 @@
 class Solution(object):
     def rotateString(self, s, goal):
-        return len(s) == len(goal) and goal in s + s
         
-        
+        if len(s) != len(goal):
+            return False
+        for i in range(len(s)):
+            rotated = s[i:] + s[:i]
+            if goal == rotated:
+                return True
+        return False
+
+# Time Complexity: O(N^2) since generating N rotations and each comparison takes O(N) time.
+
+# Space Complexity: O(N) for the space needed to store each rotated string.
