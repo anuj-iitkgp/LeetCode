bf9bc8ec81dd5bf1b86455fcc9a60f4cb3bd6bd1
Anuj Yadav <anujyiitk@gmail.com>
2026-08-09 18:12:34 +0530

    Time: 983 ms (75.15%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -3,5 +3,5 @@ SELECT e.name, b.bonus
 FROM Employee AS e
 LEFT JOIN Bonus AS b
 ON e.empId = b.empId
-WHERE b.bonus < 1000 OR b.bonus IS NULL
+WHERE  e.empId = b.empId AND b.bonus < 1000 OR b.bonus IS NULL
 
