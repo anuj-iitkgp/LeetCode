a4573a8f36959ac13a021460244740084791ea79
Anuj Yadav <anujyiitk@gmail.com>
2026-08-10 01:10:25 +0530

    Time: 13 ms (27.45%), Space: 13.5 MB (62.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,6 +1,17 @@
 from collections import Counter
 class Solution(object):
     def isIsomorphic(self, s, t):
-        return [s.find(ch) for ch in s] == [t.find(ch) for ch in t]
         
+        mapST, mapTS = {}, {}
+
+        for i in range(len(s)):
+            c1, c2 = s[i], t[i]
+            if (c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1):
+                return False
+            mapST[c1] = c2
+            mapTS[c2] = c1
+        return True
+
+# time: O(n), Space: O(m)
+# Where n is the length of the input string and m is the number of unique characters in the strings.
         
