66168b900570da543297207d09207619ac9308d1
Anuj Yadav <anujyiitk@gmail.com>
2026-08-11 19:06:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (19.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,5 +1,23 @@
 class Solution(object):
     def sortColors(self, nums):
-        return nums.sort()
+       zero, one, two = 0, 0, 0
+       n = len(nums)
 
-        
+       for i in range(n):
+        if nums[i] == 0:
+            nums[two] = 2
+            nums[one] = 1
+            nums[zero] = 0
+            two += 1
+            one += 1
+            zero += 1
+        elif nums[i] == 1:
+            nums[two ] = 2
+            nums[one] = 1
+            two += 1
+            one += 1
+        else:
+            nums[two] = 2
+            two += 1
+
+# time O(n), Space O(1)
