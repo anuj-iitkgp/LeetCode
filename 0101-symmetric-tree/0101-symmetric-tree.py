83f9f5836ae7d5acd3be935f8e35009bb72b48cb
Anuj Yadav <anujyiitk@gmail.com>
2026-08-17 09:01:12 +0530

    Time: 0 ms (100%), Space: 12.5 MB (65.51%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -2,15 +2,19 @@ class Solution(object):
     def isSymmetric(self, root):
         if not root:
             return True
+        
+        q = deque([(root.left, root.right)])
 
-        def isMirror(t1, t2):
+        while q:
+            t1, t2 = q.popleft()
+
             if not t1 and not t2:
-                return True
-            if not t1 or not t2:
+                continue
+            
+            if not t1 or not t2 or t1.val != t2.val:
                 return False
             
-            return ((t1.val == t2.val) and 
-                   isMirror(t1.left, t2.right) and 
-                   isMirror(t1.right, t2.left))
-
-        return isMirror(root.left, root.right)
+            # add mirror pairs to queue
+            q.append((t1.left, t2.right))
+            q.append((t1.right, t2.left))
+        return True
