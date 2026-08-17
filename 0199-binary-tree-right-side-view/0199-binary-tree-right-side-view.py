e16fb2c4a4d3be01e3494c596a664c8349dcafe5
Anuj Yadav <anujyiitk@gmail.com>
2026-08-17 09:13:11 +0530

    Time: 0 ms (100%), Space: 12.6 MB (0.19%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,29 +1,19 @@
-from collections import deque
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
-    def rightSideView(self, root):
-        if not root:
-            return []
-        res = []
-        q = deque([root])
 
-        while q:
-            level_length = len(q)
-            
-            for i in range(level_length):
-                node = q.popleft()
+  def rightSideView(self, root):
+    res = []
 
-                if i == level_length - 1:
-                    res.append(node.val)
-                
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-        return res
+    def dfs(node, depth):
+      if not node:
+        return
+
+      # First time visiting this depth level
+      if depth == len(res):
+        res.append(node.val)
+
+      # Prioritize right side first
+      dfs(node.right, depth + 1)
+      dfs(node.left, depth + 1)
+
+    dfs(root, 0)
+    return res
