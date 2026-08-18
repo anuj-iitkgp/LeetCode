f1f802ceb92ea236c7e4b883637167454a80cf5c
Anuj Yadav <anujyiitk@gmail.com>
2026-08-18 17:39:22 +0530

    Time: 0 ms (100%), Space: 15.1 MB (74.29%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def lemonadeChange(self, bills):
+
+        five, ten = 0, 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if five:
+                    five -= 1
+                    ten += 1
+                else:
+                    return False
+            else:
+                if five and ten:
+                    five -= 1
+                    ten -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
