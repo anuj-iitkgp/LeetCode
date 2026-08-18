5bc49fe7a736b6ae67a75c85e43198f1b8cef96c
Anuj Yadav <anujyiitk@gmail.com>
2026-08-18 18:38:49 +0530

    Time: 39 ms (46.26%), Space: 13.3 MB (61%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,20 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        currentReach = 0
-        last = len(nums) - 1
+        # currentReach = 0
+        # last = len(nums) - 1
         
-        for i in range(len(nums)):
-            # If current index is unreachable, stop
-            if i > currentReach:
-                return False
+        # for i in range(len(nums)):
+        #     if i > currentReach:
+        #         return False
             
-            # Update the furthest reachable index
-            if i + nums[i] > currentReach:
-                currentReach = i + nums[i]
+        #     if i + nums[i] > currentReach:
+        #         currentReach = i + nums[i]
                 
-            # If we can reach the last index, return True
-            if currentReach >= last:
-                return True
+        #     if currentReach >= last:
+        #         return True
                 
-        return False
+        # return False
+
+        n = len(nums)
+        maxJump = 0
+        for i in range(n):
+            if i > maxJump:
+                return False
+            maxJump = max(maxJump, i + nums[i])
+        return True
