9954cca5924ef872365e555f59beacdf9c105b3b
Anuj Yadav <anujyiitk@gmail.com>
2026-08-21 00:30:30 +0530

    Time: 0 ms (100%), Space: 12.6 MB (60.27%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,13 +1,24 @@
 class Solution(object):
     def nextGreaterElement(self, nums1, nums2):
-        res = [-1] * len(nums1)
-        for i in range(len(nums1)):
-            idx = nums2.index(nums1[i])
-            for j in range(idx + 1, len(nums2)):
-                if nums1[i] < nums2[j]:
-                    res[i] = nums2[j]
-                    break
-        return res
+        # Time Complex: O(n * m)
+        # res = [-1] * len(nums1)
+        # for i in range(len(nums1)):
+        #     idx = nums2.index(nums1[i])
+        #     for j in range(idx + 1, len(nums2)):
+        #         if nums1[i] < nums2[j]:
+        #             res[i] = nums2[j]
+        #             break
+        # return res
+        # optimal
+
+        next_greater = {}
+        stack = []
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_greater[stack.pop()] = num
+            stack.append(num)
         
+        return [next_greater.get(x, -1) for x in nums1]
             
-        
+# Time: O(m + n), Space: O(m)
