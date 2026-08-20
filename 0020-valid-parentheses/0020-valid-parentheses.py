56143883179798e18502701d8261878320083ad2
Anuj Yadav <anujyiitk@gmail.com>
2026-08-20 23:04:59 +0530

    Time: 0 ms (100%), Space: 12.5 MB (72.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,40 @@
+# class Solution(object):
+#     def isValid(self, s):
+#         n = len(s)
+#         stack = []
+
+#         for i in range(n):
+#             if s[i] == "(" or s[i] == "[" or s[i] == "{":
+#                 stack.append(s[i])
+#             else:
+#                 if len(stack) == 0:
+#                     return False
+#                 char = stack[-1]
+#                 stack.pop()
+
+#                 if not ((s[i] == ")" and char == "(") or (s[i] == "]" and char == "[") or (s[i] == "}" and char == "{")):
+#                     return False
+#         return len(stack) == 0
+
 class Solution(object):
     def isValid(self, s):
-        n = len(s)
         stack = []
+        
+        # Map matching closing brackets to opening brackets
+        mapping = {")": "(", "]": "[", "}": "{"}
 
-        for i in range(n):
-            if s[i] == "(" or s[i] == "[" or s[i] == "{":
-                stack.append(s[i])
+        for char in s:
+            if char in mapping:
+                # Pop from stack if non-empty, otherwise assign a dummy value
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
             else:
-                if len(stack) == 0:
-                    return False
-                char = stack[-1]
-                stack.pop()
+                stack.append(char)
 
-                if not ((s[i] == ")" and char == "(") or (s[i] == "]" and char == "[") or (s[i] == "}" and char == "{")):
-                    return False
         return len(stack) == 0
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 
 # Time and Space Complexity: O(n)
