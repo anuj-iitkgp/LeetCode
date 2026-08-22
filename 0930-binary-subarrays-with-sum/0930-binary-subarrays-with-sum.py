e5ccea31d854503a9d23bddfcb613d6611613d77
Anuj Yadav <anujyiitk@gmail.com>
2026-08-22 16:50:07 +0530

    Time: 40 ms (40.47%), Space: 12.8 MB (87.51%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,20 +1,25 @@
 class Solution(object):
     def numSubarraysWithSum(self, nums, goal):
+        def SubarraysLessThanGoal(nums, goal):
+            n = len(nums)
+            
+            l, r = 0, 0
+            sum1, count = 0, 0
 
-        res, currSum = 0, 0
-        prefixSums = {0:1}
+            while r < n:
+                if goal < 0:
+                    return 0
+                sum1 += nums[r]
+                while sum1 > goal:
+                    sum1 -= nums[l]
+                    l += 1
+                count += r - l + 1
+                r += 1
+            return count
 
-        for num in nums:
-            currSum += num
-            diff = currSum - goal
+        p = SubarraysLessThanGoal(nums, goal) 
+        q = SubarraysLessThanGoal(nums, goal - 1)
+        return (p - q)
 
-            res += prefixSums.get(diff, 0)
-            prefixSums[currSum] = 1 + prefixSums.get(currSum, 0)
-
-        return res
-
-
-#brute force; Time:O(n^2) and constant space
-        
-        
+# Time Com: O(1), Space: O(1)
         
