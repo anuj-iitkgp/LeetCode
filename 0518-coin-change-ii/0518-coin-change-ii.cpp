0f0cb53c301caefcedd6da9dd35b7313c6779105
Anuj Yadav <anujyiitk@gmail.com>
2026-08-25 03:18:48 +0530

    Time: 732 ms (5%), Space: 385.1 MB (5.69%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+int a;
+
+private:
+
+    int h(int i, int t, vector<int>& c, vector<vector<int>> &dp){
+        if(i==c.size() || t>a) return 0;
+        if(t==a) return 1;
+        if(dp[i][t]!=-1) return dp[i][t];
+        int take=h(i, t+c[i], c,dp);
+        int ntake=h(i+1, t, c,dp);
+        return dp[i][t]=take+ntake;
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+     vector<vector<int>> dp(301, vector<int>(50001, -1));
+    a=amount;
+        
+        return h(0, 0, coins,dp);
+    }
+};
