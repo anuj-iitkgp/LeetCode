e79d5bdda68532be01803a9f26f83fb4d5eb6f8f
Anuj Yadav <anujyiitk@gmail.com>
2026-08-25 00:28:25 +0530

    Time: 0 ms (100%), Space: 21.7 MB (6.54%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,8 +1,25 @@
 class Solution(object):
     def peakIndexInMountainArray(self, arr):
         n = len(arr)
-        ans = float('inf')
-        for i in range(1, n - 1):
-            if arr[i - 1] < arr[i] and arr[i] > arr[ i + 1]:
-                ans = min(ans, i)
-        return ans
+        # ans = float('inf')
+        # for i in range(1, n - 1):
+        #     if arr[i - 1] < arr[i] and arr[i] > arr[ i + 1]:
+        #         ans = min(ans, i)
+        # return ans
+        l, r = 0, n - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if mid == 0:
+                return mid + 1
+            if arr[mid - 1] < arr[mid] and arr[mid] > arr[mid + 1]:
+                return mid
+            elif arr[mid - 1] > arr[mid]:
+                r = mid - 1
+            elif arr[mid] < arr[mid + 1]:
+                l = mid + 1
+            
+            
+
+
+    
+        
