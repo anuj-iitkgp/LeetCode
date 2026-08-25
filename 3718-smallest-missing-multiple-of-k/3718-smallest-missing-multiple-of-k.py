5cbacc1c05a60bb4db1ecd7c6451c831e866b668
Anuj Yadav <anujyiitk@gmail.com>
2026-08-25 22:57:27 +0530

    Time: 0 ms (100%), Space: 12.4 MB (50.57%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -1,19 +1,24 @@
 class Solution(object):
     def missingMultiple(self, nums, k):
+        i = 1
+        while True:
+            if k * i not in nums:
+                return k * i
+            i += 1
 
-        n = len(nums)
-        if k == nums[0] and n == 1:
-            return 2 * k
+        # n = len(nums)
+        # if k == nums[0] and n == 1:
+        #     return 2 * k
         
-        if nums[0] % k == 0 and n == 1:
-            return k
+        # if nums[0] % k == 0 and n == 1:
+        #     return k
         
-        ans = {index : value for index, value in enumerate(nums)}
+        # ans = {index : value for index, value in enumerate(nums)}
         
-        for key, val in ans.items():
-            if k * (key + 1) not in ans.values():
-                return k * (key + 1)
-                break
-        return k * (n + 1)
+        # for key, val in ans.items():
+        #     if k * (key + 1) not in ans.values():
+        #         return k * (key + 1)
+        #         break
+        # return k * (n + 1)
         
         
