64cb553f2a32ead9e2f794c0b92ccf75274082de
Anuj Yadav <anujyiitk@gmail.com>
2026-08-27 01:47:33 +0530

    Time: 0 ms (100%), Space: 12.4 MB (23.23%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution(object):
+    def areAlmostEqual(self, s1, s2):
+        c = 0
+        if len(s1)!=len(s2):
+             return False
+        if Counter(s1) != Counter(s2):
+            return False
+        for i in range(len(s1)):
+            if(s1[i]!=s2[i]):
+                c += 1
+        if c <= 2:
+             return True
+        return False
+
+        
