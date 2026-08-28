d8315fc3ef9f4a9431b21605bae3180d2c1b568a
Anuj Yadav <anujyiitk@gmail.com>
2026-08-29 00:33:43 +0530

    Time: 51 ms (71.55%), Space: 27 MB (18.89%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,19 +1,37 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        n = len(nums)
-        if not nums:
-            return 0
-        nums.sort()
-        max_streak = 1
-        current_streak = 1
+        # n = len(nums)
+        # if not nums:
+        #     return 0
+        # nums.sort()
+        # max_streak = 1
+        # current_streak = 1
 
-        for i in range(n - 1):
-            if nums[i + 1] == nums[i]:
-                continue
-            if nums[i + 1] - nums[ i ] == 1:
-                current_streak += 1
-            else:
-                max_streak = max(max_streak, current_streak)
+        # for i in range(n - 1):
+        #     if nums[i + 1] == nums[i]:
+        #         continue
+        #     if nums[i + 1] - nums[ i ] == 1:
+        #         current_streak += 1
+        #     else:
+        #         max_streak = max(max_streak, current_streak)
+        #         current_streak = 1
+        # return max(max_streak, current_streak)
+
+        num_set = set(nums)  # O(n) space and time build
+        longest_streak = 0
+
+        for num in num_set:
+        # Only start counting if 'num' is the beginning of a sequence
+
+            if num - 1 not in num_set:
+                current_num = num
                 current_streak = 1
-        return max(max_streak, current_streak)
-        
+
+            # Count consecutive numbers
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
