2110b7cd528c92a57e3b7156e849d02d74036449
Anuj Yadav <anujyiitk@gmail.com>
2026-08-29 00:46:00 +0530

    Time: 10 ms (65.41%), Space: 13.1 MB (72.19%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,23 +1,34 @@
 class Solution(object):
     def maxProduct(self, nums):
 
-        if not nums:
-            return 0
+        # if not nums:
+        #     return 0
         
-        curr_max = nums[0]
-        curr_min = nums[0]
-        ans = nums[0]
+        # curr_max = nums[0]
+        # curr_min = nums[0]
+        # ans = nums[0]
+
+        # n = len(nums)
+
+        # for i in range(1, n):
+        #     num = nums[i]
+
+        #     if num < 0:
+        #         curr_max, curr_min = curr_min, curr_max
+            
+        #     curr_max = max(curr_max * num, num)
+        #     curr_min = min(curr_min * num, num)
+
+        #     ans = max(ans, curr_max)
+        # return ans
 
         n = len(nums)
+        ans = nums[0]
+        left_product, right_product = 0, 0
 
-        for i in range(1, n):
-            num = nums[i]
+        for i in range(n):
+            left_product = (left_product or 1) * nums[i]
+            right_product = (right_product or 1) * nums[n - i - 1]
 
-            if num < 0:
-                curr_max, curr_min = curr_min, curr_max
-            
-            curr_max = max(curr_max * num, num)
-            curr_min = min(curr_min * num, num)
-
-            ans = max(ans, curr_max)
+            ans = max(ans, left_product, right_product)
         return ans
