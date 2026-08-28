73aafff360e68ba77aee613c27d0fd1a4aee0118
Anuj Yadav <anujyiitk@gmail.com>
2026-08-29 00:49:45 +0530

    Time: 3 ms (99.27%), Space: 13.1 MB (56.26%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def maxProduct(self, nums):
+# class Solution(object):
+#     def maxProduct(self, nums):
 
         # if not nums:
         #     return 0
@@ -22,13 +22,39 @@ class Solution(object):
         #     ans = max(ans, curr_max)
         # return ans
 
-        n = len(nums)
-        ans = nums[0]
-        left_product, right_product = 0, 0
+        # n = len(nums)
+        # ans = nums[0]
+        # left_product, right_product = 0, 0
 
-        for i in range(n):
-            left_product = (left_product or 1) * nums[i]
-            right_product = (right_product or 1) * nums[n - i - 1]
+        # for i in range(n):
+        #     left_product = (left_product or 1) * nums[i]
+        #     right_product = (right_product or 1) * nums[n - i - 1]
 
-            ans = max(ans, left_product, right_product)
+        #     ans = max(ans, left_product, right_product)
+        # return ans
+
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans=nums[0]
+        maxp=nums[0]
+        minp=nums[0]
+        n=len(nums)
+        for i in range(1,n):
+            if nums[i]<0:
+                maxp,minp=minp,maxp
+            if nums[i]>maxp*nums[i]:
+                maxp=nums[i]
+            else:
+                maxp=maxp*nums[i]
+            if nums[i]<minp*nums[i]:
+                minp=nums[i]
+            else:
+                minp=minp*nums[i]
+            if(ans<maxp):
+                ans=maxp
         return ans
