6954e16d7f647387ed69413e8e9100b019c7132f
Anuj Yadav <anujyiitk@gmail.com>
2026-08-29 20:57:02 +0530

    Time: 71 ms (89.27%), Space: 12.4 MB (81.41%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -2,11 +2,19 @@ from collections import Counter
 class Solution(object):
     def repeatedStringMatch(self, a, b):
         
-        repeat = len(b) // len(a)
-        cnt = 1
+        # repeat = len(b) // len(a)
+        # cnt = 1
 
-        while cnt <= repeat + 2:
-            if b in a * cnt:
-                return cnt
-            cnt += 1
+        # while cnt <= repeat + 2:
+        #     if b in a * cnt:
+        #         return cnt
+        #     cnt += 1
+        # return -1
+
+        min_repeat = (len(b) + len(a) - 1) // len(a)
+
+        if b in a * min_repeat:
+            return min_repeat
+        if b in a * (min_repeat + 1):
+            return (min_repeat + 1)
         return -1
