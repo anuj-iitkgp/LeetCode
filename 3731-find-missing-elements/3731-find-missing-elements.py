c4c5316ba6acac5e3913fe035f2914cd4e607add
Anuj Yadav <anujyiitk@gmail.com>
2026-09-03 15:52:10 +0530

    Time: 0 ms (100%), Space: 12.2 MB (99.76%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -3,12 +3,7 @@ class Solution(object):
         max_el = max(nums)
         min_el = min(nums)
         nums_set = set(nums)
-        ans = []
-        for i in range(min_el, max_el + 1):
-            if i not in nums_set:
-                ans.append(i)
-        
-        return ans
+        return [x for x in range(min_el, max_el + 1) if x not in nums_set]
 
 
 
