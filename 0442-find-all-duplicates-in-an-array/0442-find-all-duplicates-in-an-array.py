bb7dc48c31e57fc7bd0f2b104c0c232e25c93396
Anuj Yadav <anujyiitk@gmail.com>
2026-09-04 00:58:26 +0530

    Time: 48 ms (41.05%), Space: 19.8 MB (53.2%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -5,9 +5,9 @@ class Solution(object):
 
         for i in range(n):
             idx = abs(nums[i]) - 1
-            nums[idx] = - nums[idx]
-            if nums[idx] > 0:
+            if nums[idx] < 0:
                 ans.append(idx + 1)
+            nums[idx] = - nums[idx]
         
         return ans
             
