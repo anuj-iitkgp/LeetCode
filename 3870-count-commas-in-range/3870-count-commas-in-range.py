def6f1c96f75adf843f0b636e3cbabe641eb3753
Anuj Yadav <anujyiitk@gmail.com>
2026-09-08 16:07:22 +0530

    Time: 4 ms (18.45%), Space: 12.2 MB (93.2%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -3,8 +3,12 @@ class Solution(object):
         
         if n < 1000:
             return 0
-        count = 0
-        for i in range(1000, 100001):
-            if n >= i:
-                count += 1
-        return count
+        
+        return (n - 999)
+        # count = 0
+        # for i in range(n + 1):
+
+        # # for i in range(1000, 100001):
+        # #     if n >= i:
+        # #         count += 1
+        # return count
