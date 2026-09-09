9cd390c2b07e06c545f6aaa066e10e5ba3e38290
Anuj Yadav <anujyiitk@gmail.com>
2026-09-09 22:12:37 +0530

    Time: 11 ms (56.77%), Space: 12.3 MB (91.61%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,12 +1,17 @@
 class Solution(object):
     def reverseDegree(self, s):
-        n = len(s)
-        ans = 0
         reversed_dict = {char: 26 - i for i, char in enumerate(string.ascii_lowercase)}
+        return sum(i * reversed_dict[char] for i, char in enumerate(s, start=1))
 
-        for i in range(1, n + 1):
-            ans += i * reversed_dict[s[i - 1]]
-        return ans
+        # n = len(s)
+        # ans = 0
+        # for i in range(1, n + 1):
+        #     ans += i * reversed_dict[s[i - 1]]
+        # return ans
+
+
+
+
 
 
             
