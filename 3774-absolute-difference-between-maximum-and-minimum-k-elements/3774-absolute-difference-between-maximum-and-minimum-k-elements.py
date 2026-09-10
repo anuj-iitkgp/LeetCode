6b3cd78511ad9d497755d4237185e2bb54a6a853
Anuj Yadav <anujyiitk@gmail.com>
2026-09-10 23:09:20 +0530

    Time: 4 ms (46.67%), Space: 12.3 MB (94.29%) - LeetHub

--- 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.py
+++ 3774-absolute-difference-between-maximum-and-minimum-k-elements/3774-absolute-difference-between-maximum-and-minimum-k-elements.py
@@ -2,7 +2,5 @@ class Solution(object):
     def absDifference(self, nums, k):
         n = len(nums)
         nums.sort()
-        sum1 = sum(nums[:k])
-        sum2 = sum(nums[n-k:n])
-        return abs(sum1 - sum2)
+        return abs(sum(nums[:k]) - sum(nums[n-k:n]))
         
