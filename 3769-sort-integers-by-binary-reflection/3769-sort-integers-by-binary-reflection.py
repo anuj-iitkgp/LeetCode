b4124b5c881c4574f18d04ee9836585f4cee7c0a
Anuj Yadav <anujyiitk@gmail.com>
2026-09-10 23:28:28 +0530

    Time: 11 ms (76.32%), Space: 12.6 MB (39.47%) - LeetHub

--- 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.py
+++ 3769-sort-integers-by-binary-reflection/3769-sort-integers-by-binary-reflection.py
@@ -2,12 +2,9 @@ from collections import Counter
 class Solution(object):
     def sortByReflection(self, nums):
         n = len(nums)
-        num = nums[:]
         ans = []
         for i in range(n):
-            num[i] = int(bin(nums[i])[2:][::-1], 2)
-            ans.append([num[i], nums[i]])
-        
+            ans.append([int(bin(nums[i])[2:][::-1], 2), nums[i]])
         ans.sort()
         for i in range(n):
             nums[i] = ans[i][1]
