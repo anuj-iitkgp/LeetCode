d4f51aba77e00c0f08016e3048d744419fc8f60c
Anuj Yadav <anujyiitk@gmail.com>
2026-09-10 14:51:31 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.75%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
@@ -1,14 +1,40 @@
 class Solution(object):
     def canBeIncreasing(self, nums):
-        n = len(nums)
-        for i in range(n):
-            new_nums = nums[:i] + nums[i+1:]
-            isInc = True
-            for j in range(1, len(new_nums)):
-                if new_nums[j - 1] >= new_nums[j]:
-                    isInc = False
-                    break
-            if isInc:
-                return True
-        return False
+        count = 0
+        
+        for i in range(1, len(nums)):
+            if nums[i - 1] >= nums[i]:
+                count += 1
+                if count > 1:
+                    return False
+                
+                if i > 1 and nums[i - 2] >= nums[i]:
+                    nums[i] = nums[i - 1]
+                    
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def canBeIncreasing(self, nums):
+#         n = len(nums)
+#         for i in range(n):
+#             new_nums = nums[:i] + nums[i+1:]
+#             isInc = True
+#             for j in range(1, len(new_nums)):
+#                 if new_nums[j - 1] >= new_nums[j]:
+#                     isInc = False
+#                     break
+#             if isInc:
+#                 return True
+#         return False
         
