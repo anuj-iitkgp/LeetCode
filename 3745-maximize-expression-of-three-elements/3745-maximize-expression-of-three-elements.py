24bc81d0160dd9cb20bd78d27cf0441c558f3dcb
Anuj Yadav <anujyiitk@gmail.com>
2026-09-10 23:32:19 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.81%) - LeetHub

--- 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.py
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def maximizeExpressionOfThree(self, nums):
-        n = len(nums)
         nums.sort()
-        return max(nums[n - 1] + nums[n - 2] - nums[n - 3],nums[n - 1] + nums[n - 2] - nums[0] )
+        return max(nums[-1] + nums[-2] - nums[-3],nums[-1] + nums[-2] - nums[0] )
         
