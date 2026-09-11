55bd49475430c79844755d7143f387751ace4737
Anuj Yadav <anujyiitk@gmail.com>
2026-09-11 17:39:09 +0530

    Time: 3 ms (32%), Space: 12.4 MB (18.4%) - LeetHub

--- 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.py
+++ 3726-remove-zeros-in-decimal-representation/3726-remove-zeros-in-decimal-representation.py
@@ -1,6 +1,4 @@
 class Solution(object):
     def removeZeros(self, n):
-        str1 = str(n)
-        ans = "".join(x for x in str1 if x != '0')
-        return int(ans)
+        return int("".join(x for x in str(n) if x != '0'))
         
