2b1248f00455962c61a2c3e20f7d0fd0c481c1ca
Anuj Yadav <anujyiitk@gmail.com>
2026-09-13 02:04:50 +0530

    Time: 0 ms (100%), Space: 12.5 MB (43.91%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,10 +1,17 @@
 class Solution(object):
     def checkIfExist(self, arr):
-        n = len(arr)
-        for i in range(n):
-            for j in range(n):
-                if i != j and arr[i] == 2 * arr[j]:
-                    return True
-                    break
-        return False 
+        seen = set()
+        for num in arr:
+            if num * 2 in seen or (num % 2 == 0 and num // 2 in seen):
+                return True
+            seen.add(num)
+        return False
+
+
+        # for i in range(n):
+        #     for j in range(n):
+        #         if i != j and arr[i] == 2 * arr[j]:
+        #             return True
+        #             break
+        # return False 
         
