f83d14feccf4584d1696ebdf4b9805cc84b88155
Anuj Yadav <anujyiitk@gmail.com>
2026-09-12 21:34:30 +0530

    Time: 239 ms (92.6%), Space: 0B (100%) - LeetHub

--- 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
+++ 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
@@ -1,3 +1,8 @@
-UPDATE Salary
-SET sex = IF(sex = 'm', 'f', 'm')
+-- UPDATE Salary
+-- SET sex = IF(sex = 'm', 'f', 'm')
 
+UPDATE Salary
+SET sex = CASE 
+    WHEN sex = 'm' THEN 'f'
+    ELSE 'm'
+END;
