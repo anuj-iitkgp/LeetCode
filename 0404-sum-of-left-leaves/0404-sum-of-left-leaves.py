45ee4afbd55e21ffcc61ba62602f93b1480d2cc9
Anuj Yadav <anujyiitk@gmail.com>
2026-09-14 23:55:53 +0530

    Time: 0 ms (100%), Space: 13.1 MB (23.2%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -1,29 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
+class Solution:
     def sumOfLeftLeaves(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        if not root:
+        if root is None:
             return 0
-        
-        total_sum = 0
-        
-        # Check if the left child exists and is a leaf node
-        if root.left and not root.left.left and not root.left.right:
-            total_sum += root.left.val
-        else:
-            # Recurse on the left subtree
-            total_sum += self.sumOfLeftLeaves(root.left)
-            
-        # Recurse on the right subtree
-        total_sum += self.sumOfLeftLeaves(root.right)
-        
-        return total_sum
-        
+
+        ans = 0
+
+        if root.left is not None:
+            if root.left.left is None and root.left.right is None:
+                ans += root.left.val
+            else:
+                ans += self.sumOfLeftLeaves(root.left)
+
+        ans += self.sumOfLeftLeaves(root.right)
+
+        return ans
