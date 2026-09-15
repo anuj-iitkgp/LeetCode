9fd75713cc0b8169bf102390503b39dbdaaeb3aa
Anuj Yadav <anujyiitk@gmail.com>
2026-09-16 00:04:01 +0530

    Time: 3 ms (97.96%), Space: 12.4 MB (93.88%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,19 +1,49 @@
+
 class Solution(object):
     def maxPalindromes(self, s, k):
         n = len(s)
-        if k == 1: return n
 
-        res = i = 0
+        def ip(i, j):
+            if j >= n:
+                return False
+            if i >= j:
+                return True
+            return ip(i + 1, j - 1) if s[i] == s[j] else False
 
-        while i <= n - k:
-            for d in (k, k + 1):
-                if i + d <= n and s[i : i + d] == s[i : i + d][::-1]:
-                    res += 1
-                    i += d
-                    break
-            else:
-                i += 1
+        result = 0
+        i = 0
+        while i < n - k + 1:
+            if ip(i, i + k - 1):
+                result += 1
+                i += k - 1
+            elif ip(i, i + k):
+                result += 1
+                i += k
+            i += 1
 
-        return res
+        return result
+
+
+
+
+
+
+# class Solution(object):
+#     def maxPalindromes(self, s, k):
+#         n = len(s)
+#         if k == 1: return n
+
+#         res = i = 0
+
+#         while i <= n - k:
+#             for d in (k, k + 1):
+#                 if i + d <= n and s[i : i + d] == s[i : i + d][::-1]:
+#                     res += 1
+#                     i += d
+#                     break
+#             else:
+#                 i += 1
+
+#         return res
 
         
