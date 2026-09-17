bce963e0e10a9bc4ecbc9362e8f392c32c480bb3
Anuj Yadav <anujyiitk@gmail.com>
2026-09-17 23:07:22 +0530

    Time: 721 ms (74.24%), Space: 18.3 MB (81.65%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,42 +1,35 @@
-class Solution(object):
-
+class Solution:
     def threeSum(self, nums):
+        nums.sort()
+        ans = []
         n = len(nums)
 
-        nums.sort()
-        res = []
-
         for i in range(n - 2):
-            # If the current number is > 0, no remaining 3 numbers can sum to 0
-            if nums[i] > 0:
-                break
 
-            # Skip duplicate first element (compare with PREVIOUS element)
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            left, right = i + 1, n - 1
+            j, k = i + 1, n - 1
 
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
 
                 if total < 0:
-                    left += 1
+                    j += 1
+
                 elif total > 0:
-                    right -= 1
+                    k -= 1
+
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
+                    ans.append([nums[i], nums[j], nums[k]])
 
-                    # Skip duplicate left and right elements
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
+                    j += 1
+                    k -= 1
 
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
 
-                    left += 1
-                    right -= 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
 
-
-
-        return res
+        return ans
