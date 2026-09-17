3c6d735ba206de1eab1dfa97d0dd8b9cc9a67820
Anuj Yadav <anujyiitk@gmail.com>
2026-09-18 00:44:08 +0530

    Time: 365 ms (18.75%), Space: 54 MB (68.75%) - LeetHub

---
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def maximumBeauty(self, items, queries):
+        # 1. Sort items by price ascending
+        items.sort(key = lambda x: x[0])
+        
+        # 2. Precompute running maximum beauty in-place
+        for i in range(1, len(items)):
+            items[i][1] = max(items[i][1], items[i - 1][1])
+            
+        ans = []
+        n = len(items)
+        
+        # 3. Custom Binary Search implementation
+        def binary_search(q):
+            left, right = 0, n - 1
+            best_idx = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                if items[mid][0] <= q:
+                    best_idx = mid   
+                    left = mid + 1
+                else:
+                    right = mid - 1  
+                    
+            return items[best_idx][1] if best_idx != -1 else 0
+
+        for q in queries:
+            ans.append(binary_search(q))
+            
+        return ans
