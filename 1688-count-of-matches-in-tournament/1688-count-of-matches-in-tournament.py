67a528b214693a3b23ccd87361803d76b1755d80
Anuj Yadav <anujyiitk@gmail.com>
2026-09-24 02:05:53 +0530

    Time: 15 ms (62.69%), Space: 12.4 MB (17.05%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -1,22 +1,45 @@
 class Solution(object):
     def numberOfMatches(self, n):
-        mat = 0
-        adv = 0
         t = 1
         if n == 1:
             return 0
         while n != 2:
             if n % 2 == 0:
-                mat = n // 2
-                t += mat
-                adv = n // 2
+                t += n // 2
                 n //= 2
             if n % 2 == 1:
-                mat = (n - 1) // 2
-                t += mat
-                adv = (n - 1) // 2 + 1
+                t += (n - 1) // 2
                 n = n // 2 + 1
-            
         return t
+
+        
+
+
+
+
+
+
+
+
+# class Solution(object):
+#     def numberOfMatches(self, n):
+#         mat = 0
+#         adv = 0
+#         t = 1
+#         if n == 1:
+#             return 0
+#         while n != 2:
+#             if n % 2 == 0:
+#                 mat = n // 2
+#                 t += mat
+#                 adv = n // 2
+#                 n //= 2
+#             if n % 2 == 1:
+#                 mat = (n - 1) // 2
+#                 t += mat
+#                 adv = (n - 1) // 2 + 1
+#                 n = n // 2 + 1
+            
+#         return t
 
         
