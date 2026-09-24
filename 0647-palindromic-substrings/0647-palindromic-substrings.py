e338d318700612020c89c9aaea311c962b1e10c8
Anuj Yadav <anujyiitk@gmail.com>
2026-09-25 01:48:07 +0530

    Time: 143 ms (91.62%), Space: 12.5 MB (41.17%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,12 +1,33 @@
+
+# class Solution(object):
+#     def countSubstrings(self, s):
+#         def isP(t):
+#             return t == t[::-1]
+        
+#         n = len(s)
+#         t = 0
+#         for i in range(n):
+#             for j in range(i, n):
+#                 if isP(s[i:j+1]):
+#                     t += 1
+#         return t
+
+
 class Solution(object):
     def countSubstrings(self, s):
-        def isP(t):
-            return t == t[::-1]
-        
+        count = 0
         n = len(s)
-        t = 0
         for i in range(n):
-            for j in range(i, n):
-                if isP(s[i:j+1]):
-                    t += 1
-        return t
+        # Odd-length palindromes 
+            l, r = i, i
+            while l >= 0 and r < n and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+        # Even-length palindromes 
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+        return count
