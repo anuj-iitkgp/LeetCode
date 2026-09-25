29b9fa4a85f87b6f0d4fed72153d7bb6b3094c3f
Anuj Yadav <anujyiitk@gmail.com>
2026-09-25 23:39:21 +0530

    Time: 81 ms (36.02%), Space: 87.4 MB (78.54%) - LeetHub

---
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <numeric>
+
+class Solution {
+public:
+    std::vector<int> getSumAbsoluteDifferences(std::vector<int>& nums) {
+        int n = nums.size();
+        
+        // Using long long to prevent integer overflow during sum operations
+        long long total_sum = 0;
+        for (int num : nums) {
+            total_sum += num;
+        }
+
+        long long left_sum = 0;
+        std::vector<int> ans(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            long long right_sum = total_sum - left_sum - nums[i];
+
+            long long left_diff = (long long)nums[i] * i - left_sum;
+            long long right_diff = right_sum - (long long)nums[i] * (n - i - 1);
+
+            ans[i] = left_diff + right_diff;
+            left_sum += nums[i];
+        }
+
+        return ans;
+    }
+};
